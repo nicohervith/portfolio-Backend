931efc851d26cdad376de687771993d90a74da99
nicohervith <niko_hervith@hotmail.com>
2023-02-07 13:32:20 -0300

    security

---
+++ src/main/java/com/portfolio/back/Security/Repository/iRolRepository.java
@@ -0,0 +1,14 @@
+
+package com.portfolio.back.Security.Repository;
+
+import com.portfolio.back.Security.Entity.Rol;
+import com.portfolio.back.Security.Enums.RolNombre;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface iRolRepository extends JpaRepository<Rol, Integer>{
+   Optional<Rol> findByRolNombre(RolNombre rolNombre);
+   
+}
---
+++ src/main/java/com/portfolio/back/Security/Service/RolService.java
@@ -0,0 +1,25 @@
+package com.portfolio.back.Security.Service;
+
+import com.portfolio.back.Security.Entity.Rol;
+import com.portfolio.back.Security.Enums.RolNombre;
+import com.portfolio.back.Security.Repository.iRolRepository;
+import jakarta.transaction.Transactional;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+//Transactional para tener los datos igual
+//Que en la db, hace un rollback que no impacta en la db
+@Transactional
+public class RolService {
+    @Autowired
+    iRolRepository irolRepository;
+    
+    public Optional<Rol> getByRolNombre(RolNombre rolNombre){
+        return irolRepository.findByRolNombre(rolNombre);
+    }
+    public void save(Rol rol){
+        irolRepository.save(rol);
+    }
+}
