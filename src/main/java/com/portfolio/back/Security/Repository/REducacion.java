4c86467c3cf1efe3fc346f89160ca7d80d16340f
nicohervith <niko_hervith@hotmail.com>
2023-02-17 20:19:29 -0300

    educacion

---
+++ src/main/java/com/portfolio/back/Security/Repository/REducacion.java
@@ -0,0 +1,14 @@
+package com.portfolio.back.Security.Repository;
+
+import com.portfolio.back.entity.Educacion;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface REducacion extends JpaRepository<Educacion, Integer>{
+    public Optional<Educacion> findByNombreE(String nombreE);
+    public boolean existsByNombreE(String nombreE);
+    
+    
+}
