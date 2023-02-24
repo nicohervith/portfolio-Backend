0e33332a30b5253fa711fb4174c03b1c47acca3a
nicohervith <niko_hervith@hotmail.com>
2023-02-24 20:00:31 -0300

    connect to front

--- src/main/java/com/portfolio/back/Controller/CEducacion.java
+++ src/main/java/com/portfolio/back/Controller/CEducacion.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/educacion")
-@CrossOrigin(origins = {"https://mgbfrontend.web.app","http://localhost:4200"})
+@CrossOrigin(origins = {"https://frontend-argprograma-b7913.web.app","http://localhost:4200"})
 public class CEducacion {
     @Autowired
     SEducacion sEducacion;
